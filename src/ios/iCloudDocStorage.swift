48f55027ae278f7d28c09f838a2fb9938937249e
rich_public@richis.online <rich_public@richis.online>
2017-08-22 02:25:47 +0100

    Added function code.

--- src/ios/iCloudDocStorage.swift
+++ src/ios/iCloudDocStorage.swift
@@ -1,42 +1,93 @@
 @objc(iCloudDocStorage) class iCloudDocStorage : CDVPlugin {
-  @objc(echo:)
-  func syncToCloud(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-
-    let msg = command.arguments[0] as? String ?? ""
-
-    if msg.characters.count > 0 {
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: msg,
-          message: msg,
-          preferredStyle: .alert
-        )
-      
-      self.viewController?.present(
-        toastController,
-        animated: true,
-        completion: nil
-      )
-
-      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        toastController.dismiss(
-          animated: true,
-          completion: nil
-        )
-      }
-        
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
+    var ubiquitousContainerURL: URL?
+    
+    @objc(initUbiquitousContainer:)
+    func initUbiquitousContainer(command: CDVInvokedUrlCommand) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let containerId = command.arguments[0] as? String
+            var pluginResult: CDVPluginResult
+            
+            // Initialise and store the ubiquitous container url
+            self.ubiquitousContainerURL = self.getUbiquitousContainerURL(containerId)
+            
+            if (self.ubiquitousContainerURL != nil) {
+                NSLog((self.ubiquitousContainerURL?.absoluteString)!)
+                
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK
+                )
+            }
+            else {
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_ERROR
+                )
+            }
+            
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
+        }
     }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
+    
+    @objc(syncToCloud:)
+    func syncToCloud(command: CDVInvokedUrlCommand) {
+        var pluginResult: CDVPluginResult?
+        
+        // Get the file to sync's url
+        let fileURLArg = command.arguments[0] as? String
+        
+        if (fileURLArg != nil) {
+            NSLog(fileURLArg!)
+            
+            // Convert fileUrl to URL
+            let fileURL = URL.init(string: fileURLArg!)
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                // Initialise and store the ubiquitous container url if necessary
+                if (self.ubiquitousContainerURL == nil) {
+                    self.ubiquitousContainerURL = self.getUbiquitousContainerURL(nil)
+                }
+                
+                // Get the destination URL of the file within the iCloud ubiquitous container
+                let fileUrlInUbiquitousContainer = self.ubiquitousContainerURL?
+                    .appendingPathComponent("Documents")
+                    .appendingPathComponent((fileURL?.lastPathComponent)!)
+                
+                do {
+                    // Tell iOS to move the file to the ubiquitous container and sync to iCloud
+                    try FileManager.default.setUbiquitous(
+                        true,
+                        itemAt: fileURL!,
+                        destinationURL: fileUrlInUbiquitousContainer!)
+                    
+                    pluginResult = CDVPluginResult(
+                        status: CDVCommandStatus_OK
+                    )
+                }
+                catch {
+                    pluginResult = CDVPluginResult(
+                        status: CDVCommandStatus_ERROR
+                    )
+                }
+                
+                self.commandDelegate!.send(
+                    pluginResult,
+                    callbackId: command.callbackId
+                )
+            }
+        }
+        else {
+            self.commandDelegate!.send(
+                CDVPluginResult(
+                    status: CDVCommandStatus_OK
+                ),
+                callbackId: command.callbackId
+            )
+        }
+    }
+    
+    private func getUbiquitousContainerURL(_ containerId: String?) -> URL {
+        return FileManager.default.url(forUbiquityContainerIdentifier: containerId)!
+    }
 }
--- www/iCloudDocStorage.js
+++ www/iCloudDocStorage.js
@@ -3,6 +3,10 @@ var iCloudDocStorage = exports;
 var exec = require('cordova/exec');
 var cordova = require('cordova');
 
+iCloudDocStorage.initUbiquitousContainer = function(containerId, success, error) {
+  exec(success, error, "iCloudDocStorage", "initUbiquitousContainer", [containerId]);
+};
+
 iCloudDocStorage.syncToCloud = function(fileUrl, success, error) {
   exec(success, error, "iCloudDocStorage", "syncToCloud", [fileUrl]);
 };
